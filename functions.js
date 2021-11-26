e059f17c59586e9370d758987b51098c55a9b211
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2021-11-25 22:55:56 -0600

    serious updates ;)

--- functions.js
+++ functions.js
@@ -1,3 +1,9 @@
+//jQuery???
+const script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 //const variables. grabbing a bunch of shit from the dom via id
 const darkMode = document.getElementById("dark-mode")
 const darkState = document.getElementById("dark-state")
@@ -9,11 +15,11 @@ const mainMenu = document.getElementById("mainmenu")
 
 //dark mode function
 darkMode.addEventListener("click", function() {
-  if (body.classList.contains("dark-mode")){
-      body.classList.remove("dark-mode")
-  } else if (body.classList.contains("bad-contrast")){
-      body.classList.remove("bad-contrast")
-      body.classList.add("dark-mode")
+  if (body.classList.contains("dark-mode")) {
+    body.classList.remove("dark-mode")
+  } else if (body.classList.contains("bad-contrast")) {
+    body.classList.remove("bad-contrast")
+    body.classList.add("dark-mode")
   } else {
     body.classList.add("dark-mode")
   }
@@ -21,22 +27,24 @@ darkMode.addEventListener("click", function() {
 
 //low contrast mode function
 badMode.addEventListener("click", function() {
-  if (body.classList.contains("bad-contrast")){
+  if (body.classList.contains("bad-contrast")) {
     body.classList.remove("bad-contrast")
-} else if (body.classList.contains("dark-mode")){
+  } else if (body.classList.contains("dark-mode")) {
     body.classList.remove("dark-mode")
     body.classList.add("bad-contrast")
-} else {
-  body.classList.add("bad-contrast")
-}
-})
-
-//show / hide menu on mobile
-mobileMenuButton.addEventListener("click",function() {
-  if (mainMenu.classList.contains("main-menu-on")) {
-    mainMenu.classList.remove("main-menu-on")
   } else {
-    mainMenu.classList.add("main-menu-on")
+    body.classList.add("bad-contrast")
   }
 })
+
+mobileMenuButton.addEventListener("click", function() {
+  if (mainMenu.style.display !== "flex") {
+    $(mainMenu).slideToggle(400)
+    mainMenu.style.display = "flex";
+  } else {
+    $(mainMenu).slideUp(400)
+    mainMenu.style.display = "none";
+  }
+});
+
 //yeah i should probably refactor some shit. dont @ me.
